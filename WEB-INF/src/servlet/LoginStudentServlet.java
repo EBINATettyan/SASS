2601488df688f69b1ba0f4469fc05771c3ed64d4
EBINAtettyan <tetsuya.ebina1016@gmail.com>
2019-07-02 17:08:33 +0900

    [BugFix]UIの変更

--- WEB-INF/src/servlet/LoginStudentServlet.java
+++ WEB-INF/src/servlet/LoginStudentServlet.java
@@ -27,9 +27,6 @@ public class LoginStudentServlet extends HttpServlet {
 		String studentId = request.getParameter("studentId");
 		String password = request.getParameter("password");
 
-		System.out.println(studentId);
-		System.out.println(password);
-
 		/*
 		 * DBにアクセス loginの確認
 		 */
